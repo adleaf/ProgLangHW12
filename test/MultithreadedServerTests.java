5af905f307b4bfc66cb0ec2a56b12f54efb928fb
Andrew <adleaf1@gmail.com>
2018-12-11 12:58:39 -0500

    Added more test cases

--- test/MultithreadedServerTests.java
+++ test/MultithreadedServerTests.java
@@ -92,6 +92,63 @@ public class MultithreadedServerTests extends TestCase {
  		assertEquals("Account C differs",53,accounts[2].getValue());
  		
      }
+     
+     @Test
+     public void testSubtract() throws IOException {    	 
+    	// initialize accounts 
+ 		accounts = new Account[numLetters];
+ 		for (int i = A; i <= Z; i++) {
+ 			accounts[i] = new Account(Z-i);
+ 		}
+ 		
+ 		MultithreadedServer.runServer("src/hw12/data/testSubtract", accounts);
+		
+ 		assertEquals("Account A differs",-1,accounts[0].getValue());
+ 		assertEquals("Account B differs",19,accounts[1].getValue());
+ 		assertEquals("Account C differs",20,accounts[2].getValue());
+ 		assertEquals("Account D differs",17,accounts[3].getValue());
+ 		
+     }
+     
+     @Test
+     public void testRotate() throws IOException {
+    	// initialize accounts 
+ 		accounts = new Account[numLetters];
+ 		for (int i = A; i <= Z; i++) {
+ 			accounts[i] = new Account(Z-i);
+ 		}			 
+ 		
+ 		MultithreadedServer.runServer("src/hw12/data/rotate", accounts);
+ 			
+ 		assertEquals("Account A differs",47,accounts[0].getValue());
+ 		assertEquals("Account B differs",45,accounts[1].getValue());
+ 		assertEquals("Account C differs",43,accounts[2].getValue());
+ 		assertEquals("Account D differs",41,accounts[3].getValue());
+ 		assertEquals("Account E differs",39,accounts[4].getValue());
+ 		assertEquals("Account F differs",37,accounts[5].getValue());
+ 		assertEquals("Account G differs",35,accounts[6].getValue());
+ 		assertEquals("Account H differs",33,accounts[7].getValue());
+ 		assertEquals("Account I differs",31,accounts[8].getValue());
+ 		assertEquals("Account J differs",29,accounts[9].getValue());
+ 		assertEquals("Account K differs",27,accounts[10].getValue());
+ 		assertEquals("Account L differs",25,accounts[11].getValue());
+ 		assertEquals("Account M differs",23,accounts[12].getValue());
+ 		assertEquals("Account N differs",21,accounts[13].getValue());
+ 		assertEquals("Account O differs",19,accounts[14].getValue());
+ 		assertEquals("Account P differs",17,accounts[15].getValue());
+ 		assertEquals("Account Q differs",15,accounts[16].getValue());
+ 		assertEquals("Account R differs",13,accounts[17].getValue());
+ 		assertEquals("Account S differs",11,accounts[18].getValue());
+ 		assertEquals("Account T differs",9,accounts[19].getValue());
+ 		assertEquals("Account U differs",7,accounts[20].getValue());
+ 		assertEquals("Account V differs",5,accounts[21].getValue());
+ 		assertEquals("Account W differs",3,accounts[22].getValue());
+ 		assertEquals("Account X differs",1,accounts[23].getValue());
+ 		assertEquals("Account Y differs",47,accounts[24].getValue());
+ 		assertEquals("Account Z differs",92,accounts[25].getValue());
+
+
+     }
 	 	  	 
 	
 }
